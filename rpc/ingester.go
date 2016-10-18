37590dcf6e7d67af885a3c049d3e2f31b61301a1
Brian Hicks <brian@brianthicks.com>
2016-10-17 23:48:59 -0500

    rpc(ingester): send data to influx

--- rpc/ingester.go
+++ rpc/ingester.go
@@ -3,17 +3,36 @@ package rpc
 import (
 	"fmt"
 	"io"
+	"time"
 
 	"github.com/asteris-llc/benchy/rpc/pb"
+	"github.com/influxdata/influxdb/client/v2"
 	"github.com/pkg/errors"
 )
 
 type ingester struct {
 	Verifier *TokenVerifier
+
+	// database stuff
+	InfluxConfig client.HTTPConfig
+	Database     string
 }
 
 func (i *ingester) AddBenchmark(stream pb.Ingester_AddBenchmarkServer) error {
 	var count uint64
+
+	influx, err := client.NewHTTPClient(i.InfluxConfig)
+	if err != nil {
+		return errors.Wrap(err, "could not get influx client")
+	}
+
+	batch, err := client.NewBatchPoints(client.BatchPointsConfig{
+		Precision: "s",
+		Database:  i.Database,
+	})
+	if err != nil {
+		return errors.Wrap(err, "could not start batch")
+	}
 
 	for {
 		benchmark, err := stream.Recv()
@@ -35,12 +54,51 @@ func (i *ingester) AddBenchmark(stream pb.Ingester_AddBenchmarkServer) error {
 			return nil
 		}
 
-		fmt.Printf("%+v\n", benchmark)
+		// add our point to the batch
+		if bench := benchmark.GetGoTestBench(); bench != nil {
+			point, err := client.NewPoint(
+				"benchmark",
+				map[string]string{
+					"project": benchmark.Project,
+					"name":    bench.Name,
+					"package": bench.Package,
+				},
+				map[string]interface{}{
+					"n":                 bench.N,
+					"nsPerOp":           bench.NsPerOp,
+					"allocedBytesPerOp": bench.AllocedBytesPerOp,
+					"allocsPerOp":       bench.AllocsPerOp,
+					"mbPerS":            bench.MbPerS,
+					"measured":          bench.Measured,
+				},
+				time.Unix(benchmark.Timestamp, 0),
+			)
+			if err != nil {
+				if inerr := stream.SendAndClose(&pb.WriteStatus{
+					Status: &pb.WriteStatus_Error{Error: err.Error()},
+				}); inerr != nil {
+					return errors.Wrap(err, "sending final status")
+				}
+				return errors.Wrap(err, "creating point")
+			}
+			batch.AddPoint(point)
+		}
+
 		count++
+	}
+
+	if err := influx.Write(batch); err != nil {
+		if inerr := stream.SendAndClose(&pb.WriteStatus{
+			Status: &pb.WriteStatus_Error{Error: err.Error()},
+		}); inerr != nil {
+			return errors.Wrap(err, "sending final status")
+		}
+
+		return errors.Wrap(err, "wrinting data points")
 	}
 	fmt.Println("")
 
-	err := stream.SendAndClose(&pb.WriteStatus{
+	er := stream.SendAndClose(&pb.WriteStatus{
 		Status: &pb.WriteStatus_Stats_{Stats: &pb.WriteStatus_Stats{
 			Written: count,
 		}},
